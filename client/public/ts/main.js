ff61b50275bf73e44b152d6f1ed245c79566a307
Anton Risberg Alaküla <anton.risberg_alakula@cs.lth.se>
2022-08-30 11:59:28 +0200

    Remove iframe solution

--- client/public/ts/main.js
+++ client/public/ts/main.js
@@ -2944,57 +2944,6 @@ define("ui/showVersionInfo", ["require", "exports", "model/repositoryUrl"], func
             // No need to poll for new versions, 'DEV' label already shown
             return;
         }
-        // const pollNewVersion = async (): Promise<'done' | 'again'> => new Promise((resolve, reject) => {
-        //   const iframe = document.createElement('iframe');
-        //   iframe.src = `${repositoryUrl}/-/raw/master/client/public/versionCheckerFragment.html`;
-        //   iframe.width = '0px';
-        //   iframe.height = '0px';
-        //   iframe.style.borderWidth = '0px';
-        //   document.body.appendChild(iframe);
-        //   iframe.onload = () => {
-        //     const nonce = `v-${(Math.random()*Number.MAX_SAFE_INTEGER)|0}`;
-        //     const listener = (event: MessageEvent) => {
-        //       switch (event.data.type) {
-        //         case 'version-fetch-result': {
-        //           if (event.data.nonce === nonce) {
-        //             window.removeEventListener('message', listener);
-        //             clearTimeout(timeout);
-        //             iframe.remove();
-        //             if (event.data.error) {
-        //               console.warn('Error when fetching version:', event.data.error);
-        //               reject('Timeout');
-        //             } else if (ourHash === event.data.result) {
-        //               // Status is clean.. for now.
-        //               // Check again (much) later
-        //               resolve('again');
-        //             } else {
-        //               console.log('New version available:', event.data.result);
-        //               const a = document.createElement('a');
-        //               a.href = `${repositoryUrl}/-/blob/master/code-prober.jar`;
-        //               a.target = '_blank';
-        //               a.text = 'New version available';
-        //               elem.appendChild(document.createElement('br'));
-        //               elem.appendChild(a);
-        //               resolve('done');
-        //             }
-        //           }
-        //           break;
-        //         }
-        //       }
-        //     };
-        //     iframe.contentWindow?.postMessage({
-        //       type: 'get-version-please',
-        //       nonce,
-        //     });
-        //     window.addEventListener('message', listener)
-        //     let timeout = setTimeout(() => {
-        //       console.log('Timed out checking version')
-        //       reject('Timeout');
-        //       window.removeEventListener('message', listener)
-        //       iframe.remove();
-        //     }, 60 * 1000);
-        //   }
-        // });
         const pollNewVersion = async () => {
             const header = await fetch(`https://code-prober.s3.eu-central-1.amazonaws.com/VERSION`);
             if (header.status !== 200) {
