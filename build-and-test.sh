c703142f13b452baa145135ca8f13ade969df77e
Anton Risberg Alaküla <anton.risberg_alakula@cs.lth.se>
2023-10-30 15:28:35 +0100

    Cause error on purpose to verify that test pipeline works

--- build-and-test.sh
+++ build-and-test.sh
@@ -60,7 +60,7 @@ echo "AddNum test suites success"
 java -jar code-prober-dev.jar --oneshot='{"type":"rpc","id":123,"data":{"type":"wsput:tunnel","session":"123","request":{"src":{"text":"1+2+3","posRecovery":"FAIL","cache":"FULL","tmpSuffix":".addnum"},"pos":4097,"type":"ListNodes"}}}' --output=oneshot_res addnum/AddNum.jar  2>/dev/null> test_log
 # Check that the returned node listing is Num->Add->Add->Program as expected
 actualNodeList=$(node -e 'console.log(JSON.stringify(JSON.parse(require("fs").readFileSync("oneshot_res")).data.value.response.nodes.map(n => n.result.type.split(".").slice(-1)[0])))')
-expectedNodeList='["Num","Add","Add","Program"]'
+expectedNodeList='["Num","Add","Add","Program","IntentionallyWrong"]'
 if [ ! "$actualNodeList" = "$expectedNodeList" ]; then
   echo "Unexpected node list from --oneshot request"
   echo "Expected $expectedNodeList"
